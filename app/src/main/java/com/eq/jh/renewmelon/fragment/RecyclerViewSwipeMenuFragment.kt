7672e550e4445e4881db07a3bcbb04ef79fd71d0
invin <invincible.it@kakaocorp.com>
2021-02-09 13:19:03 +0900

    리사이클러뷰 스와이프 메뉴

--- app/src/main/java/com/eq/jh/renewmelon/fragment/RecyclerViewSwipeMenuFragment.kt
+++ app/src/main/java/com/eq/jh/renewmelon/fragment/RecyclerViewSwipeMenuFragment.kt
@@ -2,9 +2,11 @@ package com.eq.jh.renewmelon.fragment
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -53,6 +55,7 @@ class RecyclerViewSwipeMenuFragment : BaseFragment() {
 
         val swipeHelperCallback = SwipeHelperCallback().apply {
             clamp = buttonWidth
+            menuMode = SwipeHelperCallback.MENU_MODE_RIGHT
         }
 
         recyclerView = view.findViewById(R.id.recycler_view)
@@ -111,11 +114,27 @@ class RecyclerViewSwipeMenuFragment : BaseFragment() {
                     val data = items[position]
 
                     vh.titleTv.text = data
+
+                    vh.left1Tv.setOnClickListener {
+                        Log.d(TAG, "left1Tv")
+                    }
+                    vh.left2Tv.setOnClickListener {
+                        Log.d(TAG, "left2Tv")
+                    }
+                    vh.imageIv.setOnClickListener {
+                        Log.d(TAG, "imageIv")
+                    }
+                    vh.itemView.setOnClickListener {
+                        Log.d(TAG, "itemView")
+                    }
                 }
             }
         }
 
         inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val left1Tv: TextView = view.findViewById(R.id.left1_tv)
+            val left2Tv: TextView = view.findViewById(R.id.left2_tv)
+            val imageIv: ImageView = view.findViewById(R.id.image_iv)
             val titleTv: TextView = view.findViewById(R.id.tv_title)
             val swipeLayout: View = view.findViewById(R.id.swipe_layout)
         }
--- app/src/main/java/com/eq/jh/renewmelon/swipe/SwipeHelperCallback2.kt
+++ app/src/main/java/com/eq/jh/renewmelon/swipe/SwipeHelperCallback2.kt
@@ -1,6 +1,7 @@
 package com.eq.jh.renewmelon.swipe
 
 import android.graphics.Canvas
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
@@ -9,24 +10,24 @@ import kotlin.math.max
 import kotlin.math.min
 
 /**
- * 참고
- * site : https://velog.io/@trycatch98/Android-RecyclerView-Swipe-Menu
- * github : https://github.com/trycatch98/RecyclerViewSwipeMenu
+ * 참고 : https://velog.io/@trycatch98/Android-RecyclerView-Swipe-Menu
  */
-class SwipeHelperCallback : ItemTouchHelper.Callback() {
+class SwipeHelperCallback2 : ItemTouchHelper.Callback() {
     companion object {
         private const val TAG = "SwipeHelperCallback"
 
-        const val MENU_MODE_RIGHT = 0
-        const val MENU_MODE_LEFT = 1
+        const val GONE = 0
+        const val LEFT_VISIBLE = 1
+        const val RIGHT_VISIBLE = 2
     }
+
+    var buttonState = GONE
+    private var preButtonState = GONE
 
     private var currentPosition: Int? = null
     private var previousPosition: Int? = null
     private var currentDx = 0f
     var clamp = 0f
-
-    var menuMode = MENU_MODE_RIGHT
 
     override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
         return makeMovementFlags(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT)
@@ -36,12 +37,14 @@ class SwipeHelperCallback : ItemTouchHelper.Callback() {
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {}
 
     override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+        Log.d(TAG, "clearView")
         currentDx = 0f
         getDefaultUIUtil().clearView(getView(viewHolder))
         previousPosition = viewHolder.adapterPosition
     }
 
     override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+        Log.d(TAG, "onSelectedChanged actionState : $actionState")
         super.onSelectedChanged(viewHolder, actionState)
         viewHolder?.let {
             currentPosition = viewHolder.adapterPosition
@@ -50,14 +53,16 @@ class SwipeHelperCallback : ItemTouchHelper.Callback() {
     }
 
     override fun getSwipeEscapeVelocity(defaultValue: Float): Float {
+        Log.d(TAG, "getSwipeEscapeVelocity")
         return defaultValue * 10
     }
 
     override fun getSwipeThreshold(viewHolder: RecyclerView.ViewHolder): Float {
+        Log.d(TAG, "getSwipeThreshold")
         val isClamped = getTag(viewHolder)
-        if (menuMode == MENU_MODE_RIGHT) {
+        if (buttonState == RIGHT_VISIBLE) {
             setTag(viewHolder, !isClamped && currentDx <= -clamp)
-        } else {
+        } else if (buttonState == LEFT_VISIBLE) {
             setTag(viewHolder, !isClamped && currentDx >= clamp)
         }
         return 2f
@@ -74,7 +79,18 @@ class SwipeHelperCallback : ItemTouchHelper.Callback() {
     }
 
     private fun clampViewPositionHorizontal(view: View, dX: Float, isClamped: Boolean, isCurrentlyActive: Boolean): Float {
-        if (menuMode == MENU_MODE_RIGHT) {
+        Log.d(TAG, "clampViewPositionHorizontal dX : $dX")
+
+        if (dX < 0f) { // 오른쪽에서 왼쪽으로 swipe : <----
+            buttonState = RIGHT_VISIBLE
+        } else if (dX > 0f) {
+            buttonState = LEFT_VISIBLE
+        } else {
+            buttonState = GONE
+        }
+
+        var returnValue = 0f
+        if (buttonState == RIGHT_VISIBLE) {
             val min: Float = -view.width.toFloat() / 2
             val max = 0f
 
@@ -83,8 +99,9 @@ class SwipeHelperCallback : ItemTouchHelper.Callback() {
             } else {
                 dX
             }
-            return min(max(min, x), max)
-        } else {
+            Log.d(TAG, "RIGHT_VISIBLE : ${min(max(min, x), max)}")
+            returnValue = min(max(min, x), max)
+        } else if (buttonState == LEFT_VISIBLE) {
             val min = 0f
             val max: Float = view.width.toFloat() / 2
 
@@ -93,8 +110,28 @@ class SwipeHelperCallback : ItemTouchHelper.Callback() {
             } else {
                 dX
             }
-            return min(max(min, x), max)
+            Log.d(TAG, "LEFT_VISIBLE : ${min(max(min, x), max)}")
+            returnValue = min(max(min, x), max)
         }
+
+        if (buttonState == GONE && preButtonState == LEFT_VISIBLE) {
+            if (isClamped) {
+                returnValue = clamp
+            } else {
+                returnValue = 0f
+            }
+        } else if (buttonState == GONE && preButtonState == RIGHT_VISIBLE) {
+            if (isClamped) {
+                returnValue = -clamp
+            } else {
+                returnValue = 0f
+            }
+        }
+        Log.d(TAG, "buttonState : ${buttonState}")
+        Log.d(TAG, "preButtonState : ${preButtonState}")
+        Log.d(TAG, "returnValue : ${returnValue}")
+        preButtonState = buttonState
+        return returnValue
     }
 
     private fun setTag(viewHolder: RecyclerView.ViewHolder, isClamped: Boolean) {
